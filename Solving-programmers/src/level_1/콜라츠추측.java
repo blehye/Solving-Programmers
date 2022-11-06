02ae52239e92380b89d3275d678dd6174b03f5c9
blehye <hhw0850@naver.com>
2022-11-06 18:30:13 +0900

    Level1 - 콜라츠 추측

--- Solving-programmers/src/level_1/콜라츠추측.java
+++ Solving-programmers/src/level_1/콜라츠추측.java
@@ -6,28 +6,27 @@ public class 콜라츠추측 {
 		int n = 6;
 		int result = solution(n);
 		System.out.println(result);
-
 	}
-	
-    public static int solution(int num2) {
-        long num = (long)num2;
-        int cnt = 0;
-		
-		while(num != 1) {
-			if(num%2 == 0) {
-				num = num/2;
+
+	public static int solution(int num2) {
+		long num = (long) num2;
+		int cnt = 0;
+
+		while (num != 1) {
+			if (num % 2 == 0) {
+				num = num / 2;
 				cnt++;
-			}else {
-				num = num*3+1;
+			} else {
+				num = num * 3 + 1;
 				cnt++;
 			}
-            if(cnt == 500) {
+			if (cnt == 500) {
 				cnt = -1;
-                break;
+				break;
 			}
-			
+
 		}
 		return cnt;
-    }
+	}
 
 }
