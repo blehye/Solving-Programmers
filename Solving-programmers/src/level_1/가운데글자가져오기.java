70a46b9e37e39f6994ff2258a04e794059969ef4
blehye <hhw0850@naver.com>
2022-11-06 18:26:21 +0900

    Level1 - 가운데 글자 가져오기

--- Solving-programmers/src/level_1/가운데글자가져오기.java
+++ Solving-programmers/src/level_1/가운데글자가져오기.java
@@ -7,20 +7,20 @@ public class 가운데글자가져오기 {
 		String result = solution(s);
 		System.out.println(result);
 	}
-	
-    public static String solution(String s) {
-        int index = 0;
+
+	public static String solution(String s) {
+		int index = 0;
 		String result = "";
-		
-		if(s.length()%2!=0) {
-			index = (s.length()-1)/2;
-			result = s.substring(index, index+1);
-		}else {
-			index = s.length()/2;
-			result = s.substring(index-1, index+1);
+
+		if (s.length() % 2 != 0) {
+			index = (s.length() - 1) / 2;
+			result = s.substring(index, index + 1);
+		} else {
+			index = s.length() / 2;
+			result = s.substring(index - 1, index + 1);
 		}
-		
-        return result;
-    }
+
+		return result;
+	}
 
 }
